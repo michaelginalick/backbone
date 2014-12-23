df8e6efc20c76713e318acd41189a5054bf0ae63
Michael <Michael.ginalick@gmail.com>
2014-12-23 16:51:14 -0600

    todo with api

---
+++ Todo-Basics-Start/js/main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+	var todoItems = new TodoItems();
+	todoItems.fetch();
+
+	
+	var todoItemsView = new TodoItemsView({ model: todoItems });
+	$("body").append(todoItemsView.render().$el);
+});
---
+++ Todo-Basics-Start/js/todoItems.js
@@ -0,0 +1,9 @@
+
+var TodoItems = Backbone.Collection.extend({
+	model: TodoItem,
+
+	url: "http://jsonplaceholder.typicode.com/todos"
+});
+
+
+
